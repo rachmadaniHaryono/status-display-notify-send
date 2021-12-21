a9b6a15eb9fe882cebe37d5d013fdacd00d7b9df
Rachmadani Haryono <foreturiga@gmail.com>
2021-12-21 12:22:54 +0800

    test: cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,6 +4,13 @@ import pytest
 
 from status_display_notify_send import cli
 
+try:
+    from status_display_notify_send.cli import DEFAULT_CMUS_INFO
+except ImportError:
+    DEFAULT_CMUS_INFO = None
+
+import pytest
+
 
 def test_main():
     """Basic CLI test."""
@@ -11,7 +18,7 @@ def test_main():
         cli.main()
 
 
-def test_show_help(capsys):
+def test_show_help():
     """
     Show help.
 
@@ -20,4 +27,18 @@ def test_show_help(capsys):
     """
     with pytest.raises(SystemExit):
         cli.main()
-    assert capsys.readouterr().out
+
+
+@pytest.mark.parametrize(
+    "cmus_data, res_update",
+    [
+        (["status", "playing", "file", "/home/user/1.mp3"], {"status": "playing"}),
+    ],
+)
+def test_status_data(cmus_data, res_update):
+    if DEFAULT_CMUS_INFO is None:
+        pytest.skip("can't get DEFAULT_CMUS_INFO")
+        return
+    exp_res = DEFAULT_CMUS_INFO.copy()
+    exp_res.update(res_update)
+    assert cli.status_data(cmus_data=cmus_data) == exp_res
